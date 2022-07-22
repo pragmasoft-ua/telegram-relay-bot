152c2f9bf81a10b9a65563dbf333d707f1e3bd36
Dmitry Zemnitskiy <pragmasoft@gmail.com>
2022-07-22 12:40:32 +0300

    Initial commit

---
+++ src/main/java/ua/pragmasoft/template/Main.java
@@ -0,0 +1,28 @@
+package ua.pragmasoft.template;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main class.
+ */
+public final class Main {
+
+  private Main() {
+
+  }
+
+  /**
+   * Logger.
+   */
+  static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+  /**
+   * Entry point.
+   *
+   * @param args arguments
+   */
+  public static void main(final String[] args) {
+    LOG.info("Pragmasoft java 17 project template");
+  }
+}
